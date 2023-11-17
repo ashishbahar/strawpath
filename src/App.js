d2655fdef8db8d9c35697bac7e838baa8988be45
ashishbahar <ashishbahar987@gmail.com>
2023-11-17 14:50:10 +0530

    main branch push our code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import logo from "../assets/images/png/logo.png";
+
+const Header = () => {
+  return (
+    <div className="header_bg">
+      <div className="d-flex pt-4 flex-column justify-content-center align-items-center w-100 ">
+        <img width={287} className="mt-3" height={155} src={logo} alt="logo" />
+        <div className=" d-flex mt-2">
+          <li>
+            <a
+              className=" ff_mont fw-semibold stroke_width_hover fs_sm text-uppercase clr_cream line_height_93"
+              href="#"
+            >
+              home
+            </a>
+          </li>
+          <li className=" mx-4 px-1">
+            <a
+              className=" ff_mont fw-semibold stroke_width_hover fs_sm text-uppercase clr_cream line_height_93"
+              href="#"
+            >
+              our story
+            </a>
+          </li>
+          <li>
+            <a
+              className=" ff_mont fw-semibold stroke_width_hover fs_sm text-uppercase clr_cream line_height_93"
+              href="#"
+            >
+              Contact
+            </a>
+          </li>
+        </div>
+        <div className=" mt-5 pt-5 ">
+          <h1 className="max_width_1433 mb-0 text-center pt-4  clr_cream ff_tenor fs_5xl text-uppercase fw-normal line_height_116">
+            Turning innovative ideas into market ready products
+          </h1>
+          <p className=" fw-medium fs_2xl clr_cream ff_mont text-center mt-3 pt-1 mb-0 max_width_1009 mx-auto line_height_108">
+            Strawpath is an innovation, solution design and product development
+            lab and consultancy
+          </p>
+          <button className=" text-center header_button ff_mont fs_xl clr_cream line_height_93 text-uppercase">
+            let’s talk
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
